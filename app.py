a0aecfc3f9267f9c6dee86ea6047e7adeb35bb1e
rizzickk <rizzickk@yahoo.com>
2025-04-21 11:03:44 -0600

    Updated script for latest deployment

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def home():
     <html>
     <head>
         <title>RS Automation</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0", user-scalable=no>
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
         <style>
